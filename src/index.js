e7ba03345c074c7a146b3900f6c560f9e3655a35
pilicarranzaa <115506676+pilicarranzaa@users.noreply.github.com>
2022-12-08 17:02:05 +0100

    Forecast created. Css updated

--- src/index.js
+++ src/index.js
@@ -1,90 +1,4 @@
-
-
-let days = [
-  `SUN`,
-  `MON`,
-  `TUE`,
-  `WED`,
-  `THU`,
-  `FRI`,
-  `SAT`
-];
-
-let months = [
-  `01`,
-  `02`,
-  `03`,
-  `04`,
-  `05`,
-  `06`,
-  `07`,
-  `08`,
-  `09`,
-  `10`,
-  `11`,
-  `12`
-];
-
-let day1 = new Date();
-
-function getDay(now) {
-  let day = days[now.getDay()];
-  let month = months[now.getMonth()];
-  let date = now.getDate();
-  let year = now.getFullYear().toString().slice(-2);
-
-  let hours = (now.getHours());
-  let minutes = (now.getMinutes());
-
-  let fullDate = document.querySelector("#fullDate");
-  fullDate.innerHTML=`${day} ${date}/${month}/${year}`;
-
-  let hour = document.querySelector("#currentHour");
-  hour.innerHTML = `${hours}:${minutes} hs`;
-  
-};
-
-getDay(day1);
-
-
-function getNextDay(day, id) {
-  day.setDate(day.getDate()+1);
-  let date = day.getDate();
-  let month = months[day.getMonth()];
-
-  let fullDate = document.querySelector(id);
-  fullDate.innerHTML=`${date}/${month}`;
-  return day;
-};
-
-let day2 = getNextDay(day1, "#day2");
-let day3 = getNextDay(day2, "#day3");
-let day4 = getNextDay(day3, "#day4");
-let day5 = getNextDay(day4, "#day5");
-let day6 = getNextDay(day5, "#day6");
-
-//-----------------------------------------
-
-addEventListener("load", searchCurrentCity);
-
-let findCity = document.querySelector("#searchCity");
-findCity.addEventListener("submit", searchCity);
-
-function searchCity(event){
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-
-  fetchCity(cityInput.value);
-}
-
-function fetchCity(city){
- return axios.get(getAPI(city)).then(showCity);
-}
-
-const getAPI = function (city) { 
-  const apiKey ="6354de4535fcb6d2655dc8e4b8d58339";
-  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-}
+// function to make background gif change according to the weather
 
 function changeBg(mainDescription, pod){
   if((mainDescription == "Thunderstorm" || mainDescription == "Drizzle" || mainDescription == "Rain") && pod == "d"){
@@ -142,6 +56,95 @@ function changeBg(mainDescription, pod){
   };
 }
 
+// getting days
+
+let days = [
+  `SUN`,
+  `MON`,
+  `TUE`,
+  `WED`,
+  `THU`,
+  `FRI`,
+  `SAT`
+];
+
+let months = [
+  `01`,
+  `02`,
+  `03`,
+  `04`,
+  `05`,
+  `06`,
+  `07`,
+  `08`,
+  `09`,
+  `10`,
+  `11`,
+  `12`
+];
+
+let currentDay = new Date();
+
+function getCurrentDay(now) {
+  let day = days[now.getDay()];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let year = now.getFullYear().toString().slice(-2);
+
+  let hours = (now.getHours());
+  let minutes = (now.getMinutes());
+
+  let fullDate = document.querySelector("#fullDate");
+  fullDate.innerHTML=`${day} ${date}/${month}/${year}`;
+
+  let hour = document.querySelector("#currentHour");
+  hour.innerHTML = `${hours}:${minutes} hs`;
+  
+};
+
+getCurrentDay(currentDay);
+
+
+function getNextDay(day, id) {
+  day.setDate(day.getDate()+1);
+  let date = day.getDate();
+  let month = months[day.getMonth()];
+
+  let fullDate = document.querySelector(id);
+  fullDate.innerHTML=`${date}/${month}`;
+  return day;
+};
+
+let day1 = getNextDay(currentDay, "#day1");
+let day2 = getNextDay(day1, "#day2");
+let day3 = getNextDay(day2, "#day3");
+let day4 = getNextDay(day3, "#day4");
+let day5 = getNextDay(day4, "#day5");
+
+// function to get the current city and the weather
+
+addEventListener("load", searchCurrentCity);
+
+let findCity = document.querySelector("#searchCity");
+findCity.addEventListener("submit", searchCity);
+
+function searchCity(event){
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+
+  fetchCity(cityInput.value);
+
+  cityInput.value = '';
+}
+
+function fetchCity(city){
+ return axios.get(getAPI(city)).then(showCity);
+}
+
+const getAPI = function (city) { 
+  const apiKey ="6354de4535fcb6d2655dc8e4b8d58339";
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+}
 
 function showCity(response) {
   let cityName = response.data.name;
@@ -188,9 +191,10 @@ function showCity(response) {
   `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", description); 
 
+  getForecast(response.data.coord);
 }
 
-//-----------------------------------
+//function to get the searched city and its weather
 
 let findButton = document.querySelector("#find");
 findButton.addEventListener("click", searchCurrentCity);
@@ -253,10 +257,12 @@ function showTemp(response) {
   iconElement.setAttribute("src", 
   `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", description); 
+
+  getForecast(response.data.coord);
 }
 
 
-//----------------------------------
+//function to convert farenheit and celsius
 
 let isCelsius = true;
 
@@ -295,3 +301,36 @@ tempC.addEventListener("click", convertToCelsius);
 
 let tempF = document.querySelector(".farenheit");
 tempF.addEventListener("click", convertToFarenheit);
+
+// function to get the forecast for the next days
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "a969311cfcbb4a83dfad2cf7478397f9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
+function displayForecast (response) {
+  let [currentDay, ...forecast] = response.data.daily;
+
+  forecast.forEach(function({ temp, weather }, index){ 
+    const dayIndex = index + 1;
+    let min = document.querySelector(`#min${dayIndex}`);
+    min.innerHTML=Math.round(temp.min);
+
+    let max = document.querySelector(`#max${dayIndex}`);
+    max.innerHTML=Math.round(temp.max);
+
+    let icon = document.querySelector(`#icon${dayIndex}`);
+    icon.setAttribute("src", 
+    `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`);
+    icon.setAttribute("alt", weather[0].description); 
+  })
+}
+
+
+
+
